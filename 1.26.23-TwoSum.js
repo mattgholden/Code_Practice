f280250ac87d7495ef37151c77532d6dfca9945a
mgholden <mgholden27@gmail.com>
2023-01-27 19:41:00 -0500

    Python approach added for TwoSum

--- 1.26.23-TwoSum.js
+++ 1.26.23-TwoSum.js
@@ -36,3 +36,12 @@ var twoSum = function(nums, target){
     }
     return ans;  
 }
+
+// class Solution:
+// def twoSum(self, nums: List[int], target: int) -> List[int]:
+// index1 = 0
+// while True:
+//     for index2 in range(index1+1,len(nums)):
+//         if nums[index1] == target - nums[index2]:
+//             return [index1,index2]       
+//     index1+=1
