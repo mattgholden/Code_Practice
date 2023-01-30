9908c4f5694a98e6e74bd9220aaab825047d3d8c
mgholden <mgholden27@gmail.com>
2023-01-29 19:23:32 -0500

    Solution added

--- 1.29.23-FindPivotIndex.js
+++ 1.29.23-FindPivotIndex.js
@@ -3,5 +3,23 @@
 // If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
 // Return the leftmost pivot index. If no such index exists, return -1.
 
+function Accumulation(arr){
+    return arr.reduce((a,b)=>a+b);  
+}
 
+var pivotIndex = function(nums) {
+    // Balance
+    let totalOnLeft = 0;
+    let totalOnRight = Accumulation(nums);
+    
+    for(let i = 0; i < nums.length; i ++){
+        let currentTotal = nums [i];
+        totalOnRight -= currentTotal;
+        if(totalOnLeft == totalOnRight){
+            return i;
+        }
+        totalOnLeft += currentTotal
+    }
+    return -1;
+};
  
