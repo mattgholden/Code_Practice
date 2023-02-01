6b733e177a8a14938f93bf9f016331016f43777a
mgholden <mgholden27@gmail.com>
2023-01-31 19:14:14 -0500

    Comments added

--- 1.31.23-IsSubsequence.js
+++ 1.31.23-IsSubsequence.js
@@ -2,3 +2,9 @@
 
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
+// Comments//
+ //Is the s string a subsequence of t string or not?
+ //True or False
+ //We want to find characters that are in the same position
+ //loop through to compare the index positions
+ 
