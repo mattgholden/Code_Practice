4fdc34854c84136e5c58aa8a2593deb53df82039
mgholden <mgholden27@gmail.com>
2023-02-02 21:50:36 -0500

    Solution added

--- 2.2.23-ReverseLinkedList.js
+++ 2.2.23-ReverseLinkedList.js
@@ -1,1 +1,16 @@
 // Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+var reverseList = function(head){
+    if(!head)
+    return null;
+
+    let preRev = head.next;
+    head.next = null;
+
+    while(preRev){
+        let rev = preRev.next;
+        head = preRev;
+        preRev = rev;
+    }
+    return head;
+}
