f8f00d3cf0f5029758a6c35ee5f47208bf7ebc00
mgholden <mgholden27@gmail.com>
2023-02-02 22:06:41 -0500

    Adding Python solution

--- 2.2.23-ReverseLinkedList.js
+++ 2.2.23-ReverseLinkedList.js
@@ -4,6 +4,7 @@ var reverseList = function(head){
     if(!head)
     return null;
 
+    //Set a variable called preRev for before reverse is complete.
     let preRev = head.next;
     head.next = null;
 
@@ -14,3 +15,15 @@ var reverseList = function(head){
     }
     return head;
 }
+
+//// Python for good measure////
+// class Solution (object):
+//      def reverseList(self, head):
+//          prev = None
+//          curr = head
+//          while curr:
+//              next = curr.next
+//              curr.next = prev
+//              prev = curr
+//              curr = next
+//          return prev
