1bdf51ac88f8cf96f9deef741ea443faf34b54a6
mgholden <mgholden27@gmail.com>
2023-02-04 23:53:55 -0500

    Solution comments/explanations added

--- 2.4.23-FizzBuzz.js
+++ 2.4.23-FizzBuzz.js
@@ -24,12 +24,17 @@
 
 /////More understandable solution///
 function fizzBuzz(n){
+    // create a variable to store an array
     let answer = [];
+    //for loop starting at index 1, if i is less than or equal to n we continue.
     for(let i = 1; i <= n; i++){
         let currentString = '';
         // console.log(currentString)
+        //if the remainder of i divided by 3 is o then create the string 'Fizz'
         if (i % 3 === 0) currentString += 'Fizz';
+        //if the remainder of i divided by 5 is 0 then create the string 'Buzz'
         if(i % 5 === 0) currentString += 'Buzz';
+        //if the current string is not a multiple of 3 or 5 (empty string) then return the value of i
         if (currentString === '') currentString += i;
 
         answer.push(currentString);
