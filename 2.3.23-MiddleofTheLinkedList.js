39da6b46332c91f63aa4a166eed1b22d8f3e8fac
mgholden <mgholden27@gmail.com>
2023-02-04 23:42:11 -0500

    Solution added

--- 2.3.23-MiddleofTheLinkedList.js
+++ 2.3.23-MiddleofTheLinkedList.js
@@ -1,3 +1,12 @@
 // Given the head of a singly linked list, return the middle node of the linked list.
 
 // If there are two middle nodes, return the second middle node.
+
+var middleNode = function(head) {
+let fast = slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+}
