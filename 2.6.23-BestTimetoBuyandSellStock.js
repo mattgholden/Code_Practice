f932cc39556eb3bbc846ca16be9cc48c581befff
mgholden <mgholden27@gmail.com>
2023-02-14 12:17:14 -0500

    Python solution

--- 2.6.23-BestTimetoBuyandSellStock.js
+++ 2.6.23-BestTimetoBuyandSellStock.js
@@ -19,3 +19,18 @@ const maxProfit = (prices) => {
     }
     return max_profit;
 }
+
+// Python Alternative//
+// class Solution:
+//     def maxProfit(self,prices):
+//         left = 0 #Buy
+//         right = 1 #Sell
+//         max_profit = 0
+//         while right < len(prices):
+//             currentProfit = prices[right] - prices[left] #our current Profit
+//             if prices[left] < prices[right]:
+//                 max_profit =max(currentProfit,max_profit)
+//             else:
+//                 left = right
+//             right += 1
+//         return max_profit
