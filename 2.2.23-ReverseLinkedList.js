bb062a7fab118202c016f7bdd2ab972788f9c6ad
mgholden <mgholden27@gmail.com>
2023-02-14 17:40:09 -0500

    ES6 for 2.2.23

--- 2.2.23-ReverseLinkedList.js
+++ 2.2.23-ReverseLinkedList.js
@@ -1,6 +1,6 @@
 // Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-var reverseList = function(head){
+const reverseList = (head) => {
     if(!head)
     return null;
 
