9e9394ec05582638beef853da08484cd4403b49a
mgholden <mgholden27@gmail.com>
2023-02-14 12:13:25 -0500

    Solution for problem 2.6.23

--- 2.6.23-BestTimetoBuyandSellStock.js
+++ 2.6.23-BestTimetoBuyandSellStock.js
@@ -3,3 +3,19 @@
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+const maxProfit = (prices) => {
+    let left = 0; //Buy
+    let right = 1; //Sell
+    let max_profit = 0;
+    while (right < prices.length){
+        if(prices[left] < prices[right]){
+            let profit = prices[right] - prices[left];
+
+            max_profit = Math.max(max_profit, profit);
+        } else {
+            left = right;
+        }
+        right++;
+    }
+    return max_profit;
+}
