ca60f09fc72a4c5cb41b55e2990a59e404d6ee91
mgholden <mgholden27@gmail.com>
2023-02-14 17:39:14 -0500

    reworked 2.1.23

--- 2.1.23-MergeTwoSortedLists.js
+++ 2.1.23-MergeTwoSortedLists.js
@@ -2,7 +2,7 @@
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 // Return the head of the merged linked list.
 
-var mergeTwoLists = function(list1, list2){
+const mergeTwoLists = (list1, list2)=>{
     let list = new ListNode(),
         head = list;
     
