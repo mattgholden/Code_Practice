fb2ca34e8519e729e74a3085970dc89bccca5d5e
mgholden <mgholden27@gmail.com>
2023-02-14 17:44:44 -0500

    Solution added for 2.7.23

--- 2.7.23-LongestPalindrome.js
+++ 2.7.23-LongestPalindrome.js
@@ -1,3 +1,15 @@
 // Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
 
 // Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+const longestPalindrome = (s) => {
+    const map = new Map();
+    for (let c of s) {
+      map.set(c, map.get(c) + 1 || 1);
+    }
+  
+    let odds = 0;
+    map.forEach((count) => (odds += count % 2));
+  
+    return s.length - odds + !!odds;
+  };
