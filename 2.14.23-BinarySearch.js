e5f3b794185fa3096c5c0b900b3b46d9a164617e
mgholden <mgholden27@gmail.com>
2023-02-14 17:35:04 -0500

    Solution added

--- 2.14.23-BinarySearch.js
+++ 2.14.23-BinarySearch.js
@@ -1,3 +1,21 @@
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 // You must write an algorithm with O(log n) runtime complexity.
+
+const search = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+    console.log(right);
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) { 
+      return middle;
+    } else if (nums[middle] < target) { 
+
+      left = middle + 1; 
+    } else {
+        right = middle - 1;
+    }
+  }
+  return -1;
+};
