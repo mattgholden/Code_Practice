762637a25b4e1fbb909d4f75d29fcb091881d7f3
mgholden <mgholden27@gmail.com>
2023-02-16 19:55:33 -0500

    Solution for 2.15.23 added

--- 2.15.23-SquaresofASortedArray.js
+++ 2.15.23-SquaresofASortedArray.js
@@ -1,1 +1,19 @@
 // Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+const sortedSquares = (nums) =>{
+    let start = 0;
+    let end = nums.length - 1;
+    let idx = end;
+    let sortPowArr = [];
+
+    while (idx > -1) {
+        if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+            sortPowArr[idx--] = nums[start] * nums[start];
+            start++;
+        } else {
+            sortPowArr[idx--] = nums[end] * nums[end];
+            end--;
+        }
+    }
+    return sortPowArr;
+};
