910a9dd07f99759a05c10138b1bfd50b8aa24a2a
mgholden <mgholden27@gmail.com>
2023-02-21 19:36:02 -0500

    Added solution for 2.17

--- 2.17.23-MoveZeroes.js
+++ 2.17.23-MoveZeroes.js
@@ -1,3 +1,14 @@
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 // Note that you must do this in-place without making a copy of the array.
+
+const moveZeroes = (nums) => {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++){
+        if (nums[j] !== 0){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+        }
+    }
+    nums.fill(0, i);
+}
