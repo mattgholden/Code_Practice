d270471a9641c5d1e2e9471fb76c38b17a7b42a0
mgholden <mgholden27@gmail.com>
2023-02-22 21:30:13 -0500

    solution for 2.20.23 added

--- 2.20.23-TwoSumII.js
+++ 2.20.23-TwoSumII.js
@@ -5,3 +5,20 @@
 // The tests are generated such that there is exactly one solution. You may not use the same element twice.
 
 // Your solution must use only constant extra space.
+
+const twoSum = (numbers, target) =>{
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right){
+        const sum = numbers[left] + numbers[right];
+        if (sum === target){
+            return [left + 1, right + 1];
+        } else if (sum < target){
+            left++;
+        } else {
+            right --;
+        }
+    }
+    return [];
+}
