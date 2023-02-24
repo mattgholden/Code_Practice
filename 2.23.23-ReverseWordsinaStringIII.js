0d5b959b2157c2ec20ce56f583120a87fcbb9227
mgholden <mgholden27@gmail.com>
2023-02-23 21:22:04 -0500

    Solution for 2.23.23

--- 2.23.23-ReverseWordsinaStringIII.js
+++ 2.23.23-ReverseWordsinaStringIII.js
@@ -1,1 +1,15 @@
 // Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+const reverseWords = (s) => {
+    // Split the string into words
+    const words = s.split(' ');
+
+    // Reverse the word
+    const reverseWords = words.map((word) => {
+        return word.split('').reverse().join('');
+    });
+    // Join reversed words back into a sentence
+    const reversedSentence = reverseWords.join('');
+
+    return reversedSentence;
+}
