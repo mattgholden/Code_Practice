159bf026771fe88002c37a57595f87a43878bf61
mgholden <mgholden27@gmail.com>
2023-02-24 21:01:10 -0500

    Add solution for 2.24.23

--- 2.24.23-RemovetheNthNodefromtheEndofaList.js
+++ 2.24.23-RemovetheNthNodefromtheEndofaList.js
@@ -1,1 +1,22 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
+const removeNthFromEnd = (head, n) => {
+    // Initialize two pointers
+    let first = head;
+    let second = head;
+    // Move the second pointer n nodes ahead of the first pointer
+    for (let i = 0; i < n; i++) {
+        second = second.next;
+    }
+    // If the second pointer is null, the node to be deleted is the head
+    if (second === null) {
+        return head.next;
+    }
+    // Move both pointers until the second pointer reaches the end of the list
+    while (second.next !== null) {
+        first = first.next;
+        second = second.next;
+    }
+    // Delete the nth node from the end of the list
+    first.next = first.next.next;
+    return head;
+};
