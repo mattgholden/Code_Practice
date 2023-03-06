a5da0146f4ac5c47c28aeeb52df996700f06ad71
mgholden <mgholden27@gmail.com>
2023-03-05 21:35:19 -0500

    Solution added

--- 3.5.23-HappyNumber.js
+++ 3.5.23-HappyNumber.js
@@ -6,3 +6,22 @@
 // Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
 // Those numbers for which this process ends in 1 are happy.
 // Return true if n is a happy number, and false if not.
+
+const isHappy = (n) => {
+let seen = new Set();
+  while (n != 1 && !seen.has(n)) {
+    seen.add(n);
+    n = sumOfSquares(n);
+  }
+  return n == 1;
+}
+
+const sumOfSquares = (n) => {
+  let sum = 0;
+  while (n > 0) {
+    let digit = n % 10;
+    sum += digit * digit;
+    n = Math.floor(n / 10);
+  }
+  return sum;
+}
