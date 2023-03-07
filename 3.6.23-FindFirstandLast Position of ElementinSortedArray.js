578d5746157e8902960e01097203f7932141de2c
mgholden <mgholden27@gmail.com>
2023-03-06 21:58:06 -0500

    Solution for 3.6.23 added

--- 3.6.23-FindFirstandLast Position of ElementinSortedArray.js
+++ 3.6.23-FindFirstandLast Position of ElementinSortedArray.js
@@ -3,3 +3,45 @@
 // If target is not found in the array, return [-1, -1].
 
 // You must write an algorithm with O(log n) runtime complexity.
+
+const searchRange = (nums, target) => {
+    let left = 0;
+ let right = nums.length - 1;
+ let start = -1;
+ 
+ // Find leftmost occurrence of target
+ while (left <= right) {
+     let mid = Math.floor((left + right) / 2);
+     if (nums[mid] >= target) {
+         right = mid - 1;
+     } else {
+         left = mid + 1;
+     }
+     if (nums[mid] === target) {
+         start = mid;
+     }
+ }
+ 
+ // Target not found
+ if (start === -1) {
+     return [-1, -1];
+ }
+ 
+ // Find rightmost occurrence of target
+ left = start;
+ right = nums.length - 1;
+ let end = start;
+ while (left <= right) {
+     let mid = Math.floor((left + right) / 2);
+     if (nums[mid] <= target) {
+         left = mid + 1;
+     } else {
+         right = mid - 1;
+     }
+     if (nums[mid] === target) {
+         end = mid;
+     }
+ }
+ 
+ return [start, end];
+};
