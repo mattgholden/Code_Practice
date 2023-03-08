eec60c68f21e1d9d40bdf8e5bc296b62ad2613f9
mgholden <mgholden27@gmail.com>
2023-03-08 18:14:08 -0500

    Up & down traversal added

--- 3.8.23-SpiralMatrix.js
+++ 3.8.23-SpiralMatrix.js
@@ -1,1 +1,25 @@
 // Given an m x n matrix, return all elements of the matrix in spiral order.
+
+const spiralOrder = (matrix) =>{
+    let result = [];
+    if (matrix.length === 0){
+        return result;
+    }
+
+    let top = 0, bottom = matrix.length - 1;
+    let left = 0, right = matrix[0].length -1;
+
+    while (top <= bottom && left <= right){
+        //Traverse right
+        for (let i = left; i <= right; i++){
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        //Traverse down
+        for (let i = top; i <= bottom; i++){
+            result.push(matrix[i][right]);
+        }
+        right--;
+    }
+}
