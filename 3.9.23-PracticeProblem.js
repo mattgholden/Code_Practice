e055f5c0203cc74ddfb09c3a5b2528980a7e6fbe
mgholden <mgholden27@gmail.com>
2023-03-09 16:57:50 -0500

    First two steps

--- 3.9.23-PracticeProblem.js
+++ 3.9.23-PracticeProblem.js
@@ -2,10 +2,14 @@
 
 // create a variable called 'sum'.
 
-// We will need to start the variable at zero and iterate through each # in the given array. We can use a for loop for this.
+const sumOfNumOverTenOrUnderFive = (nums) =>{
+    let sum = 0;
 
+// We will need to start the variable at zero and iterate through each # in the given array. We can use a for loop for this.
+    for (let i = 0; i < nums.length; i++);
 // With an if statement we will check whether each element is greater than 10 or less than 5.
 
 // If the element meets the above criteria the function adds the element's value to the a 'sum' variable.
 
 // If the function returns the value of 'sum'.  This will be the sum of all the elements in the array that are either > 10 or < 5.
+}
