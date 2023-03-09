3e7575d42d27df32e4aab809dd5d5160e774b613
mgholden <mgholden27@gmail.com>
2023-03-09 16:50:17 -0500

    added more steps to the solution

--- 3.9.23-PracticeProblem.js
+++ 3.9.23-PracticeProblem.js
@@ -1,3 +1,5 @@
 // Write a function that takes in a list or an array of numbers. The function should return the sum of any numbers from the list that are either greater than 10 or less than 5
 
-// We will need to start with zero and iterate through each # in the given array.
+// We will need to start with zero and iterate through each # in the given array.  We can use a for loop for this.
+
+// With an if statement we will check whether each element is greater than 10 or less than 5.
