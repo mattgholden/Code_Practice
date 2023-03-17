4f601b219d7c81138d9a9c471c8b47a544d60755
mgholden <mgholden27@gmail.com>
2023-03-16 23:21:52 -0400

    Solution for 3.15 added

--- 3.15.23-SearchA2DMatrix.js
+++ 3.15.23-SearchA2DMatrix.js
@@ -6,3 +6,25 @@
 
 // You must write a solution in O(log(m * n)) time complexity.
 
+const searchMatrix = (matrix, target) => {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const num = matrix[Math.floor(mid / n)][mid % n];
+
+        if (num === target) {
+            return true;
+        } else if (num < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
