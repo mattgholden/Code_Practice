5fe93d97efc1284d40ded254a80876d71b653809
mgholden <mgholden27@gmail.com>
2023-03-22 20:24:35 -0400

    Solution for 3.20 added

--- 3.20.23-RemoveDuplicatesfromSortedListII.js
+++ 3.20.23-RemoveDuplicatesfromSortedListII.js
@@ -1,1 +1,26 @@
 // Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+function deleteDuplicates(head) {
+    if (!head || !head.next) {
+      return head;
+    }
+  
+    let sample = new ListNode(-1); // create a sample node to handle edge cases
+    sample.next = head;
+    let prev = sample;
+    let curr = head;
+  
+    while (curr) {
+      if (curr.next && curr.val === curr.next.val) {
+        while (curr.next && curr.val === curr.next.val) {
+          curr = curr.next;
+        }
+        prev.next = curr.next;
+      } else {
+        prev = curr;
+      }
+      curr = curr.next;
+    }
+  
+    return sample.next;
+  }
