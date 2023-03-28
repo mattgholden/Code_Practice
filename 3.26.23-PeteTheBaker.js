13e7859b3c69a6310a15f9b225f7ff47025f0594
mgholden <mgholden27@gmail.com>
2023-03-27 22:40:09 -0400

    Added solution for 3.26.23

--- 3.26.23-PeteTheBaker.js
+++ 3.26.23-PeteTheBaker.js
@@ -2,3 +2,13 @@
 
 // Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
 
+const cakes = (recipe, available) => {
+    let maxCakes = Infinity;
+    for (let ingredient in recipe) {
+      if (!(ingredient in available)) {
+        return 0;
+      }
+      maxCakes = Math.min(maxCakes, Math.floor(available[ingredient] / recipe[ingredient]));
+    }
+    return maxCakes;
+  }
