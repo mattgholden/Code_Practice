4cd9d839374c71f346e66333ce4358621f000f31
mgholden <mgholden27@gmail.com>
2023-04-03 11:52:17 -0400

    solution for 3.31 added

--- 3.31.23-SplitStrings.js
+++ 3.31.23-SplitStrings.js
@@ -1,2 +1,14 @@
 // Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
+const solution = (str) => {
+    var i = 0;
+    var result = new Array();
+    if (str.length % 2 !== 0) {
+      str = str + '_';
+    }
+    while (i < str.length) {
+        result.push(str[i] + str[i+1]);
+        i += 2;
+      }
+    return result;
+  }
